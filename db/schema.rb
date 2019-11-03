814fc5e071fcb04f66c4b4f198eb5bea378fbfaf
Matthew McCormack <mmccormack1182@gmail.com>
2019-11-03 15:03:17 -0500

    removed user table and began installing Devise (config.action_mailer)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_23_172602) do
+ActiveRecord::Schema.define(version: 2019_11_03_195509) do
 
   create_table "checks", force: :cascade do |t|
     t.string "name"
@@ -38,14 +38,6 @@ ActiveRecord::Schema.define(version: 2019_10_23_172602) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "email"
-    t.string "password_digest"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
 end
